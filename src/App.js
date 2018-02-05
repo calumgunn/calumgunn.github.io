59839d1ae825a0f74bcc13a46e0bdfe47e366be7
Calum Gunn <calum@calumgunn.com>
2018-02-05 21:43:16 +0100

    Nice styling

--- src/App.js
+++ src/App.js
@@ -15,14 +15,18 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <div>
-            <nav>
-              <Link to="/">Cameron Shafii</Link>
-              <Link to="/releases">Releases</Link>
-              <Link to="/performances">Performances</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
-            </nav>
-            <div>
+            <header>
+              <h1>
+                <Link to="/">Cameron Shafii</Link>
+              </h1>
+            </header>
+            <div className="app">
+              <nav>
+                <Link to="/releases">Releases</Link>
+                <Link to="/performances">Performances</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+              </nav>
               <Route exact path="/" component={Home} />
               <Route path="/releases" component={Releases} />
               <Route path="/performances" component={Performances} />
