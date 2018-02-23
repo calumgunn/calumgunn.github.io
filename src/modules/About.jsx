d22f0ac9e87acbca58d9b7edc6f9187177d549ab
corporavilia <36713332+corporavilia@users.noreply.github.com>
2018-02-23 08:43:56 -0800

    Update About.jsx

--- src/modules/About.jsx
+++ src/modules/About.jsx
@@ -9,9 +9,23 @@ export default class About extends PureComponent {
 
         <img src={Cameron} alt="Cameron Shafii" />
 
-        <p>Cameron Shafii is an Iranian composer practicing electronic and electroacoustic music. His compositions are inflected with a host of digital synthesis processes and are informed by aspects of acousmatic theory, particularly spectromorphology. His works do not impose any temporal scheme upon the listener - rejecting the semiotic operation of 'engagement/disengagement'. ‘Pithy & Prolix’ and ‘Corpora Vilia’ - his recent pieces published on the Anòmia and Conditional labels - present asymmetrical narratives that articulate tension, amorphous forms, and complex sonorities, signifying a plurality of meaning and complication.</p>
-        
-		<p>Shafii has performed at concerts, festivals, and workshops, and has exhibited pieces as sound installations in the US and Canada. He runs the Ge-stell label and is based in Philadelphia, where he is pursuing a Ph.D. in German at the University of Pennsylvania.</p>
+        <p>Cameron Shafii is an Iranian composer practicing generative and
+        electroacoustic music. His compositions are inflected with a host of
+        digital synthesis processes and are informed by aspects of acousmatic
+        theory, particularly spectromorphology. His works do not impose any
+        temporal scheme upon the listener - rejecting the semiotic operation of
+        'engagement/disengagement'.</p>
+
+        <p>'Pithy & Prolix' - his forthcoming piece for the Anòmia label -
+        features sound scenes that emphasize the materiality and morphology of
+        spatial textures. In it, Shafii presents an asymmetrical narrative that
+        articulates tension, amorphous forms, and complex sonorities,
+        signifying a plurality of meaning and complication.</p>
+
+        <p>Shafii has performed at concerts, festivals, installations, and
+        workshops in the US and Canada, and has collaborated on an ongoing
+        basis with dancer Beth Graczyk. He runs the Ge-stell label and is based
+        in Durham, North Carolina.</p>
       </section>
     );
   }
