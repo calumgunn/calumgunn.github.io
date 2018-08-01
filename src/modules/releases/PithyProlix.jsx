17c946230db159e72aa9dd3e1d4df1db44d339c5
corporavilia <36713332+corporavilia@users.noreply.github.com>
2018-08-01 14:41:57 -0400

    Update PithyProlix.jsx

--- src/modules/releases/PithyProlix.jsx
+++ src/modules/releases/PithyProlix.jsx
@@ -20,19 +20,11 @@ export default class PithyProlix extends PureComponent {
 
         <h3>Tracklist:</h3>
 
-        <p>01 Pithy & Prolix [40:48]</p>
-
-        <p>i. kreischen schreien zersplittertes glas, Op. 37<br />
-        ii. Qui parle<br />
-        iii. Infiltration homogène pour piano à droit<br />
-        iv. Mouvement (un nœud de significations vivantes)<br />
-        v. The theory of the body is already a theory of perception<br />
-        vi. Postludium</p>
+        <p>01. Pithy & Prolix [40:48]</p>
 
         <p>Mastered by Noel Summerville at 3345 Mastering, London.</p>
 
-        <p>Special thanks to Kirsten Thom, Arnau Sala Saez, Noel Summerville,
-        Kevin Drumm, RM Francis, Joe Gilmore, and John Wall.</p>
+        <p>Special thanks to Kirsten Thom, Arnau Sala Saez, and Kirsten Thom.</p>
       </section>
     );
   }
