59900908008799ffdc903f462108c0e6708c9253
corporavilia <36713332+corporavilia@users.noreply.github.com>
2019-02-10 11:23:40 -0500

    Update About.jsx
    Update About.jsx

--- src/modules/About.jsx
+++ src/modules/About.jsx
@@ -16,7 +16,7 @@ export default class About extends PureComponent {
 
         <p>Cameron Shafii is an Iranian composer practicing electronic and electroacoustic music. His compositions are inflected with a host of digital synthesis processes and are informed by aspects of acousmatic theory, particularly spectromorphology. His works have been released by various music labels, including Anòmia and Conditional.</p>
 
-        <p>Shafii runs the Ge-stell label, and is based in Philadelphia, where is pursuing a Ph.D. in Germanic Languages and Literatures and the History of Art at the University of Pennsylvania.</p>
+        <p>Shafii runs the Ge-stell label and is based in Philadelphia, where is pursuing a Ph.D. in Germanic Languages and Literatures and the History of Art at the University of Pennsylvania.</p>
 
       </section>
     );
