deef21ca835ae886a189bad0fa0bdcc2845551fa
corporavilia <36713332+corporavilia@users.noreply.github.com>
2019-03-22 17:13:33 -0400

    Update Performances.jsx

--- src/modules/Performances.jsx
+++ src/modules/Performances.jsx
@@ -41,9 +41,9 @@ export default class Performances extends PureComponent {
         02/24/2018 - Seattle, WA at West Coasting Printing, Inc., Corridor Festival 2018</p>
 
         <p>(w/ RM Francis)<br />
-        10/20/2017 - Baltimore, MD at The Red Room, Diffusion Festival 2017<br />
+        10/22/2017 - Queens, NY at H0L0<br />
         10/21/2017 - Philadelphia, PA at Icebox Project Space, Human Festival 2017<br />
-        10/22/2017 - Queens, NY at H0L0</p>
+        10/20/2017 - Baltimore, MD at The Red Room, Diffusion Festival 2017</p>
 
         <p>(w/ Archivist, IVVY)<br />
         08/11/2017 - Naches, WA at American Ridge Lodge, Lodge Festival 2017</p>
@@ -55,9 +55,9 @@ export default class Performances extends PureComponent {
         05/06/2017 - Seattle, WA at Chapel Performance Space, Draft</p>
 
         <p>(w/ Kevin Drumm)<br />
-        01/20/2017 - Vancouver, BC at Red Gate Revue Stage, Quiet City #31<br />
+        01/22/2017 - Portland, OR at S1, 3 Day Festival 2017<br />
         01/21/2017 - Seattle, WA at Georgetown Steam Plant, Corridor Festival 2017<br />
-        01/22/2017 - Portland, OR at S1, 3 Day Festival 2017</p>
+        01/20/2017 - Vancouver, BC at Red Gate Revue Stage, Quiet City #31</p>
 
         <p>(w/ Daniel Menche)<br />
         12/22/2016 - Seattle, WA at Chapel Performance Space</p>
@@ -75,14 +75,14 @@ export default class Performances extends PureComponent {
         06/10/2016 - Portland, OR at S1</p>
 
         <p>(w/ CoH)<br />
-        03/11/2016 - San Francisco, CA at F8, Surface Tension<br />
-        03/12/2016 - Los Angeles, CA at Human Resources<br />
-        03/13/2016 - Phoenix, AZ at The Rebel Lounge<br />
-        03/15/2016 - Oakland, CA at The Night Light<br />
-        03/17/2016 - Seattle, WA at Kremwerk, Elevator<br />
+        03/19/2016 - Portland, OR at S1, 2 Day Festival 2016<br />
         03/18/2016 - Portland, OR at S1, 2 Day Festival 2016<br />
-        03/19/2016 - Portland, OR at S1, 2 Day Festival 2016</p>
-
+        03/17/2016 - Seattle, WA at Kremwerk, Elevator<br />
+        03/15/2016 - Oakland, CA at The Night Light<br />
+        03/13/2016 - Phoenix, AZ at The Rebel Lounge<br />
+        03/12/2016 - Los Angeles, CA at Human Resources<br />
+        03/11/2016 - San Francisco, CA at F8, Surface Tension</p>
+        
         <p>(w/ William Huston & Ted Byrnes, VRS)<br />
         01/22/2016 - Los Angeles, CA at The Wulf</p>
 
@@ -90,31 +90,32 @@ export default class Performances extends PureComponent {
         03/14/2015 - San Francisco, CA at The Lab</p>
 
         <p>(w/ Scald Rougish)<br />
-        02/10/2015 - San Francisco, CA at Elbo Room<br />
-        02/13/2015 - Oakland, CA at Terminal<br />
-        02/14/2015 - Los Angeles, CA at Complex<br />
         02/15/2015 - Phoenix, AZ at Apartment 512 Gallery, Ascetic House</p>
+        02/14/2015 - Los Angeles, CA at Complex<br />
+        02/13/2015 - Oakland, CA at Terminal<br />
+        02/10/2015 - San Francisco, CA at Elbo Room</p>
 
         <p>(in Sutekh Hexen)<br />
-        08/26/2012 - Oakland, CA at Terminal<br />
-        08/28/2012 - Denver, CO at Aqualung<br />
-        08/29/2012 - Iowa City, IA at Gabe's<br />
-        08/30/2012 - Madison, WI at Dragonfly Lounge<br />
-        08/31/2012 - Chicago, IL at The Burlington<br />
-        09/01/2012 - Cleveland, OH at Now That's Class<br />
-        09/02/2012 - Philadelphia, PA at Millcreek Tavern<br />
-        09/03/2012 - Pawtucket, RI at Machines With Magnets<br />
-        09/04/2012 - Brooklyn, NY at Saint Vitus Bar<br />
-        09/05/2012 - Richmond, VA at Strange Matter<br />
-        09/06/2012 - Stevenson, MD at Greenspring Art Gallery, Stevenson University<br />
-        09/08/2012 - Raleigh, NC at Hopscotch Music Festival 2012 (w/ Sunn O))), Kevin Drumm)<br />
-        09/09/2012 - New Orleans, LA at The Big Top<br />
-        09/11/2012 - Denton, TX at Rubber Glove<br />
-        09/13/2012 - Albuquerque, NM at Small Engine Room<br />
-        09/14/2012 - Tempe, AZ at Yucca Tap Room<br />
-        09/15/2012 - Los Angeles, CA at Vacation Vinyl<br />
+        09/23/2012 - Soda Springs, CA at Stella Natura Festival 2012 (w/ Aluk Todolo, Arktau Eos)<br />
         09/20/2012 - Oakland, CA at Oakland Metro Operahouse<br />
-        09/23/2012 - Soda Springs, CA at Stella Natura Festival 2012 (w/ Aluk Todolo, Arktau Eos)</p>
+        09/15/2012 - Los Angeles, CA at Vacation Vinyl<br />
+        09/14/2012 - Tempe, AZ at Yucca Tap Room<br />
+        09/13/2012 - Albuquerque, NM at Small Engine Room<br />
+        09/11/2012 - Denton, TX at Rubber Glove<br />
+        09/09/2012 - New Orleans, LA at The Big Top<br />
+        09/08/2012 - Raleigh, NC at Hopscotch Music Festival 2012 (w/ Sunn O))), Kevin Drumm)<br />
+        09/06/2012 - Stevenson, MD at Greenspring Art Gallery, Stevenson University<br />
+        09/05/2012 - Richmond, VA at Strange Matter<br />
+        09/04/2012 - Brooklyn, NY at Saint Vitus Bar<br />
+        09/03/2012 - Pawtucket, RI at Machines With Magnets<br />
+        09/02/2012 - Philadelphia, PA at Millcreek Tavern<br />
+        09/01/2012 - Cleveland, OH at Now That's Class<br />
+        08/31/2012 - Chicago, IL at The Burlington<br />
+        08/30/2012 - Madison, WI at Dragonfly Lounge<br />
+        08/29/2012 - Iowa City, IA at Gabe's<br />
+        08/28/2012 - Denver, CO at Aqualung<br />
+        08/26/2012 - Oakland, CA at Terminal</p>
+      
       </section>
     );
   }
